70769160a5c7dfc802e5a82b78925fdb3dc4e86d
Matt Hartley <m4tthartley@gmail.com>
2025-09-29 01:37:34 +0100

    FillTriangle shading

--- main.c
+++ main.c
@@ -58,7 +58,52 @@ uint32_t __deltaTime;
 #define RDP_COMB_PRIMITIVE		3
 #define RDP_COMB_SHADE			4
 #define RDP_COMB_ENVIRONMENT	5
-#define RDP_COMB_ZERO			16 // dunno if this is right
+
+#define RDP_COMB_A_ONE			6
+#define RDP_COMB_A_NOISE		7
+#define RDP_COMB_A_ZERO			8
+#define RDP_COMB_A_ALPHA_ONE	6
+#define RDP_COMB_A_ALPHA_ZERO	7
+
+#define RDP_COMB_B_CENTER		6
+#define RDP_COMB_B_K4			7
+#define RDP_COMB_B_ZERO			8
+#define RDP_COMB_B_ALPHA_ONE	6
+#define RDP_COMB_B_ALPHA_ZERO	7
+
+#define RDP_COMB_C_SCALE				6
+#define RDP_COMB_C_COMBINED_ALPHA 		7
+#define RDP_COMB_C_TEX0_ALPHA			8
+#define RDP_COMB_C_TEX1_ALPHA			9
+#define RDP_COMB_C_PRIMITIVE_ALPHA		10
+#define RDP_COMB_C_SHADE_ALPHA			11
+#define RDP_COMB_C_ENVIRONMENT_ALPHA	12
+#define RDP_COMB_C_LOD_FRACTION			13
+#define RDP_COMB_C_PRIM_LOD_FRAC		14
+#define RDP_COMB_C_K5					15
+#define RDP_COMB_C_ZERO					16
+#define RDP_COMB_C_ALPHA_PRIM_LOD_FRAC	6
+#define RDP_COMB_C_ALPHA_ZERO			7
+
+#define RDP_COMB_D_ONE			6
+#define RDP_COMB_D_ZERO			7
+#define RDP_COMB_D_ALPHA_ONE	6
+#define RDP_COMB_D_ALPHA_ZERO	7
+
+// #define RDP_COMB_1CYCLE_RGB(a, b, c, d) (((uint64_t)a<<37) | ((uint64_t)c<<32) | ((uint64_t)b<<24) | ((uint64_t)d<<6))
+
+uint64_t RDP_CombinerRGB(uint64_t a, uint64_t b, uint64_t c, uint64_t d)
+{
+	uint64_t result = 0;
+	if (!a) a = RDP_COMB_A_ZERO;
+	if (!b) b = RDP_COMB_B_ZERO;
+	if (!c) c = RDP_COMB_C_ZERO;
+	if (!d) d = RDP_COMB_D_ZERO;
+	result = (a<<37) | (c<<32) | (b<<24) | (d<<6);
+	return result;
+}
+
+// #define RDP_COMB_ZERO			16 // dunno if this is right
 
 // uint64_t raw;
 // struct {
@@ -105,10 +150,98 @@ void MemoryBarrier()
 	asm volatile ("" : : : "memory");
 }
 
+typedef int16_t fixed16_t; // fixed point s8.8
+typedef int32_t fixed32_t; // fixed point s16.16
+typedef int16_t fixed12_4_t; // fixed point s12.4
+
+typedef union {
+	struct {
+		fixed32_t x, y, z, w;
+	};
+	struct {
+		fixed32_t r, g, b, a;
+	};
+} vec4fixed32_t;
+
+typedef struct {
+	struct {
+		uint8_t x, y, z, w;
+	};
+	struct {
+		uint8_t r, g, b, a;
+	};
+} vec4u8_t;
+
+typedef struct {
+	float x, y;
+} vec2_t;
+
+typedef union {
+	struct {
+		float x, y, z;
+	};
+	struct {
+		float r, g, b;
+	};
+} vec3_t;
+
+typedef union {
+	struct {
+		float x, y, z, w;
+	};
+	struct {
+		float r, g, b, a;
+	};
+} vec4_t;
+
+vec4_t vec4(float x, float y, float z, float w)
+{
+	return (vec4_t){x, y, z, w};
+}
+vec4_t vec4f(float f)
+{
+	return (vec4_t){f, f, f, f};
+}
+vec4_t add4(vec4_t a, vec4_t b)
+{
+	return (vec4_t){a.x+b.x, a.y+b.y, a.z+b.z, a.w+b.w};
+}
+vec4_t sub4(vec4_t a, vec4_t b)
+{
+	return (vec4_t){a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w};
+}
+vec4_t mul4(vec4_t a, vec4_t b)
+{
+	return (vec4_t){a.x*b.x, a.y*b.y, a.z*b.z, a.w*b.w};
+}
+vec4_t mul4f(vec4_t a, float f)
+{
+	return (vec4_t){a.x*f, a.y*f, a.z*f, a.w*f};
+}
+vec4_t div4(vec4_t a, vec4_t b)
+{
+	return (vec4_t){a.x/b.x, a.y/b.y, a.z/b.z, a.w/b.w};
+}
+vec4_t div4f(vec4_t a, float f)
+{
+	return (vec4_t){a.x/f, a.y/f, a.z/f, a.w/f};
+}
+
+vec4fixed32_t vec4tofixed32(vec4_t v)
+{
+	v = mul4f(v, 65536.0f);
+	return (vec4fixed32_t){ v.x, v.y, v.z, v.w };
+}
+
 typedef struct {
 	uint32_t x;
 	uint32_t y;
 } vecscreen_t; // Currently just 32bit uint
+
+typedef struct {
+	vec2_t pos;
+	vec4_t color;
+} rdp_vertex_t;
 
 typedef struct {
 	void* buffer;
@@ -130,6 +263,12 @@ void RDP_Write(rdpcmdlist_t* cmdlist, uint32_t word)
 	uint32_t* buffer = (uint32_t*)((uint8_t*)cmdlist->buffer + cmdlist->cursor);
 	*buffer = word;
 	cmdlist->cursor += 4;
+}
+
+void RDP_SetCombineMode(rdpcmdlist_t* cmdlist, uint64_t combinerMask)
+{
+	RDP_Write(cmdlist, (0x3C<<24) | (combinerMask >> 32));
+	RDP_Write(cmdlist, (combinerMask & 0xFFFFFFFF));
 }
 
 void RDP_FillTriangle(rdpcmdlist_t* cmdlist, vecscreen_t* verts)
@@ -171,16 +310,20 @@ void RDP_FillTriangle(rdpcmdlist_t* cmdlist, vecscreen_t* verts)
 	RDP_Write(cmdlist, dxmdy);
 }
 
-void RDP_FillTriangleWithShade(rdpcmdlist_t* cmdlist, vecscreen_t* verts)
+void RDP_FillTriangleWithShade(rdpcmdlist_t* cmdlist, rdp_vertex_t* verts)
 {
-	if (verts[0].y > verts[1].y) swap(verts[0], verts[1]);
-	if (verts[1].y > verts[2].y) swap(verts[1], verts[2]);
-	if (verts[0].y > verts[1].y) swap(verts[0], verts[1]);
+	rdp_vertex_t v0 = verts[0];
+	rdp_vertex_t v1 = verts[1];
+	rdp_vertex_t v2 = verts[2];
+
+	if (v0.pos.y > v1.pos.y) swap(v0, v1);
+	if (v1.pos.y > v2.pos.y) swap(v1, v2);
+	if (v0.pos.y > v1.pos.y) swap(v0, v1);
 
 	rdpcommand_t cmd;
-	int32_t v0x = verts[0].x, v0y = verts[0].y;
-	int32_t v1x = verts[1].x, v1y = verts[1].y;
-	int32_t v2x = verts[2].x, v2y = verts[2].y;
+	int32_t v0x = v0.pos.x, v0y = v0.pos.y;
+	int32_t v1x = v1.pos.x, v1y = v1.pos.y;
+	int32_t v2x = v2.pos.x, v2y = v2.pos.y;
 	int32_t lydiff = v2y-v1y;
 	int32_t hydiff = v2y-v0y;
 	int32_t mydiff = v1y-v0y;
@@ -210,50 +353,162 @@ void RDP_FillTriangleWithShade(rdpcmdlist_t* cmdlist, vecscreen_t* verts)
 	RDP_Write(cmdlist, dxmdy);
 
 	// SHADE
-	int r = 0;
-	int g = 0;
-	int b = 0;
-	int a = 0;
 
+#if 0
 	// int part of shade color at xh, floor(yh)
 	RDP_Write(cmdlist, (64<<16) | 64);
 	RDP_Write(cmdlist, (64<<16) | 64);
-
 	// int part change in shade along scanline
-	// int16_t change = -10;
-	// RDP_Write(cmdlist, ((uint32_t)change<<16) | change);
-	// RDP_Write(cmdlist, ((uint32_t)change<<16) | change);
 	RDP_Write(cmdlist, (4<<16));
 	RDP_Write(cmdlist, 0);
-
 	// fractional part of shade color at xh, floor(yh)
-	// RDP_Write(cmdlist, (10<<16) | 10);
-	// RDP_Write(cmdlist, (10<<16) | 10);
 	RDP_Write(cmdlist, 0);
 	RDP_Write(cmdlist, 0);
-
 	// fractional part change in shade along scanline
-	// RDP_Write(cmdlist, (((uint32_t)-1)<<16) | ((uint32_t)-1));
-	// RDP_Write(cmdlist, (((uint32_t)-1)<<16) | ((uint32_t)-1));
 	RDP_Write(cmdlist, 0);
 	RDP_Write(cmdlist, 0);
-
 	// int part change along major edge
 	RDP_Write(cmdlist, (((uint32_t)-1)<<16));
 	RDP_Write(cmdlist, 0);
-
 	// int part change each scanline
-	// RDP_Write(cmdlist, (((uint32_t)2)<<16));
 	RDP_Write(cmdlist, 0);
 	RDP_Write(cmdlist, 0);
-
 	// frac part change along major edge
 	RDP_Write(cmdlist, 0);
 	RDP_Write(cmdlist, 0);
-
 	// frac part change each scanline
 	RDP_Write(cmdlist, 0);
 	RDP_Write(cmdlist, 0);
+#endif
+
+	/*
+		{x = 200, y = 50, C = 240 (just red component to test)}
+		{x = 260, y = 100, C = 120 (just red component to test)}
+		{x = 230, y = 150, C = 60 (just red component to test)}
+
+		hx = 230-200 = 30
+		hy = 150-50 = 100
+		mx = 260-200 = 60
+		my = 100-50 = 50
+
+		mr = -120;
+		hr = -180;
+
+		nxr = hy*mr - my*hr = -12,000 - -9,000 = -3,000
+
+		const float nz = (data->hx*data->my) - (data->hy*data->mx); = -4,500
+    	data->attr_factor = -1.0f / nz; = -0.0002222222222
+
+		DrDx = nxr * attr_factor = 0.6666666666
+
+		{
+			hr/hx = -180 / 30 = -6
+			mr/mx = -120 / 60 = -2
+		}
+
+		{
+			dx1 = 260-200 = 60
+			dy1 = 100-50 = 50
+			dx2 = 230-200 = 30
+			dy2 = 150-50 = 100
+
+			dc1 = -120;
+			dc2 = -180;
+
+			det = dx1*dy2 - dx2*dy1 = 4500
+			a = (dc1*dy2 - dc2*dy1) / det = -0.6666666667
+			b = (dx1*dc2 - dx2*dc1) / det = -1.6
+			c = c0 - a*x0 - b*y0 = 453.33333334
+			edgeVector = (230-200) / (150-50) = 0.3
+
+			dedge = b + a * edgeVector = -1.8
+		}
+	*/
+
+	// Long edge = v0->v2, longest edge
+	// High edge = v0->v1, high edge of the triangle arm
+	// Low edge = v1->v2, low edge of the triangle arm
+
+	// float HighEdgeColorDelta = v1.color.x - v0.color.x;
+	vec4_t highEdgeColorDelta = sub4(v1.color, v0.color);
+	// float longEdgeColorDelta = v2.color.x - v0.color.x;
+	vec4_t longEdgeColorDelta = sub4(v2.color, v0.color);
+
+	float longDiffX = v2x-v0x;
+	float longDiffY = v2y-v0y;
+	float highDiffX = v1x-v0x;
+	float highDiffY = v1y-v0y;
+	float lowDiffX = v2x-v1x;
+	float lowDiffY = v2y-v1y;
+
+	float determinant = highDiffX*longDiffY - longDiffX*highDiffY;
+
+	vec4_t xCoef = div4f(sub4(mul4f(highEdgeColorDelta, longDiffY), mul4f(longEdgeColorDelta, highDiffY)), determinant);
+	vec4_t yCoef = div4f(sub4(mul4f(longEdgeColorDelta, highDiffX), mul4f(highEdgeColorDelta, longDiffX)), determinant);
+	vec4_t edgeCoef = add4(yCoef, mul4f(xCoef, (longDiffX/longDiffY)));
+
+	// xCoef = -2.0f;
+	// yCoef = -2.0f;
+	// edgeCoef = -2.0f;
+
+	// xCoef *= 65536.0f;
+	// xCoef = mul4f(xCoef, 65536.0f);
+	// yCoef *= 65536.0f;
+	// yCoef = mul4f(yCoef, 65536.0f);
+	// edgeCoef *= 65536.0f;
+	// edgeCoef = mul4f(edgeCoef, 65536.0f);
+
+	// float f = 65536.0f;
+	// int32_t xCoefFixed = xCoef;
+	vec4fixed32_t xCoefFixed = vec4tofixed32(xCoef);
+	// int32_t yCoefFixed = yCoef;
+	vec4fixed32_t yCoefFixed = vec4tofixed32(yCoef);
+	// int32_t edgeCoefFixed = edgeCoef;
+	vec4fixed32_t edgeCoefFixed = vec4tofixed32(edgeCoef);
+
+	// vec4fixed32_t startColor = vec4tofixed32(v0.color);
+	uint32_t r = ((uint32_t)v0.color.r) & 0xFF;
+	uint32_t g = ((uint32_t)v0.color.g) & 0xFF;
+	uint32_t b = ((uint32_t)v0.color.b) & 0xFF;
+	uint32_t a = ((uint32_t)v0.color.a) & 0xFF;
+
+	// int part of shade color at xh, floor(yh)
+	RDP_Write(cmdlist, (r<<16) | g);
+	RDP_Write(cmdlist, (b<<16) | a);
+	// int part change in shade along scanline
+	RDP_Write(cmdlist, (xCoefFixed.r&0xFFFF0000) | ((xCoefFixed.g>>16)&0xFFFF));
+	RDP_Write(cmdlist, (xCoefFixed.b&0xFFFF0000) | ((xCoefFixed.a>>16)&0xFFFF));
+	// fractional part of shade color at xh, floor(yh)
+	RDP_Write(cmdlist, 0);
+	RDP_Write(cmdlist, 0);
+	// fractional part change in shade along scanline
+	RDP_Write(cmdlist, (xCoefFixed.r<<16) | (xCoefFixed.g&0xFFFF));
+	RDP_Write(cmdlist, (xCoefFixed.b<<16) | (xCoefFixed.a&0xFFFF));
+	// int part change along major edge
+	RDP_Write(cmdlist, (edgeCoefFixed.r&0xFFFF0000) | ((edgeCoefFixed.g>>16)&0xFFFF));
+	RDP_Write(cmdlist, (edgeCoefFixed.b&0xFFFF0000) | ((edgeCoefFixed.a>>16)&0xFFFF));
+	// int part change each scanline
+	RDP_Write(cmdlist, (yCoefFixed.r&0xFFFF0000) | ((yCoefFixed.g>>16)&0xFFFF));
+	RDP_Write(cmdlist, (yCoefFixed.b&0xFFFF0000) | ((yCoefFixed.a>>16)&0xFFFF));
+	// frac part change along major edge
+	RDP_Write(cmdlist, (edgeCoefFixed.r<<16) | (edgeCoefFixed.g&0xFFFF));
+	RDP_Write(cmdlist, (edgeCoefFixed.b<<16) | (edgeCoefFixed.a&0xFFFF));
+	// frac part change each scanline
+	RDP_Write(cmdlist, (yCoefFixed.r<<16) | (yCoefFixed.g&0xFFFF));
+	RDP_Write(cmdlist, (yCoefFixed.b<<16) | (yCoefFixed.a&0xFFFF));
+
+	// // int part change along major edge
+	// RDP_Write(cmdlist, 0);
+	// RDP_Write(cmdlist, 0);
+	// // int part change each scanline
+	// RDP_Write(cmdlist, 0);
+	// RDP_Write(cmdlist, 0);
+	// // frac part change along major edge
+	// RDP_Write(cmdlist, 0);
+	// RDP_Write(cmdlist, 0);
+	// // frac part change each scanline
+	// RDP_Write(cmdlist, 0);
+	// RDP_Write(cmdlist, 0);
 }
 
 void RDP_FillRect(rdpcmdlist_t* cmdlist, int32_t x, int32_t y, int32_t width, int32_t height)
@@ -596,7 +851,7 @@ int main()
 
 			// Set Primitive Color
 			RDP_Write(&cmdlist, 0x3A<<24);
-			RDP_Write(&cmdlist, 0xFF0350FF);
+			RDP_Write(&cmdlist, 0xFFFFFFFF);
 
 			// Set Environment Color
 			RDP_Write(&cmdlist, 0x3B<<24);
@@ -605,18 +860,28 @@ int main()
 			// Set Combine Mode
 			// RDP_Write(&cmdlist, (0x3C<<24) | (RDP_COMB_PRIMITIVE<<20) | (RDP_COMB_SHADE<<15) | (RDP_COMB_PRIMITIVE<<12) | (RDP_COMB_SHADE<<9));
 			// RDP_Write(&cmdlist, 0);
-			RDP_Write(&cmdlist, (0x3C<<24) | (RDP_COMB_PRIMITIVE<<20) | (RDP_COMB_SHADE<<15) | (RDP_COMB_PRIMITIVE<<5) | (RDP_COMB_SHADE<<0));
-			RDP_Write(&cmdlist, (8<<24) | (7<<6));//(RDP_COMB_PRIMITIVE<<12) | (RDP_COMB_SHADE<<9)
+
+			// RDP_Write(&cmdlist, (0x3C<<24) | (RDP_COMB_PRIMITIVE<<20) | (RDP_COMB_SHADE<<15) | (RDP_COMB_PRIMITIVE<<5) | (RDP_COMB_SHADE<<0));
+			// RDP_Write(&cmdlist, (8<<24) | (7<<6));//(RDP_COMB_PRIMITIVE<<12) | (RDP_COMB_SHADE<<9)
+			// uint64_t combine = RDP_CombinerRGB(RDP_COMB_PRIMITIVE, 0, RDP_COMB_SHADE, 0);
+			// RDP_Write(&cmdlist, (0x3C<<24) | (combine >> 32));
+			// RDP_Write(&cmdlist, (combine & 0xFFFFFFFF));
+			RDP_SetCombineMode(&cmdlist, RDP_CombinerRGB(RDP_COMB_PRIMITIVE, 0, RDP_COMB_SHADE, 0));
 
 			// Pipe Sync
 			RDP_Write(&cmdlist, 0x27000000);
 			RDP_Write(&cmdlist, 0);
 
 			// Shaded triangle
-			vecscreen_t verts[] = {
-				{200, 50},
-				{260, 100},
-				{230, 150},
+			// rdp_vertex_t verts[] = {
+			// 	{{200, 50}, {255, 255, 255, 255}},
+			// 	{{260, 100}, {30, 120, 120, 255}},
+			// 	{{230, 150}, {30, 10, 200, 255}},
+			// };
+			rdp_vertex_t verts[] = {
+				{{200, 50}, {0, 0, 255, 255}},
+				{{260, 100}, {255, 0, 0, 255}},
+				{{230, 150}, {0, 255, 0, 255}},
 			};
 			RDP_FillTriangleWithShade(&cmdlist, verts);
 
